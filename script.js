7c6b22ac9e094fb7954d48abd09b092d0bf48c5b
krystyna-lyn <67439674+krystyna-lyn@users.noreply.github.com>
2023-11-28 13:01:26 +0000

    update projects

--- script.js
+++ script.js
@@ -122,21 +122,3 @@ for (let i = 0; i < filterBtn.length; i++) {
     lastClickedBtn = filterBtn[i];
   });
 }
-
-// Contact
-
-const form = document.querySelector("[data-form]");
-const formInputs = document.querySelectorAll("[data-form-input]");
-const formBtn = document.querySelector("[data-form-btn]");
-
-// add event to all form input field
-for (let i = 0; i < formInputs.length; i++) {
-  formInputs[i].addEventListener("input", () => {
-    // check form validation
-    if (form.checkValidity()) {
-      formBtn.removeAttribute("disabled");
-    } else {
-      formBtn.setAttribute("disabled", "");
-    }
-  });
-}
